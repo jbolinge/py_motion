a2e94dc937f360dfd4500a12e6c8a42c2041eadc
me <me@pluto.com>
2015-10-02 15:05:29 -0500

    .

--- pymotion.py
+++ pymotion.py
@@ -1,45 +1,81 @@
 #! /usr/bin/python
 
-import smtplib
+import smtplib, multiprocessing
 import datetime as dt
 import imutils, time, json, warnings
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
-def run_motion(camera, conf):
+def run_motion(conf):
+	#camera = cv2.VideoCapture(0)
+	camera = PiCamera()
+	camera.resolution = tuple(conf["resolution"])
+	camera.framerate = conf["fps"]
+	rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+	#print "Warming up"
+	time.sleep(conf["camera_warmup_time"])
 	avg = None
-	while True:
-		(grabbed, frame) = camera.read()
+	last_MMS = dt.datetime.now()
+	last_save = dt.datetime.now()
+	motion_counter = 0
+	#while True:
+	for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+		frame = f.array
+		#(grabbed, frame) = camera.read()
+		#if not grabbed:
+		#	break
+
+		timestamp = dt.datetime.now()
 		text = "Unoccupied"
-		if not grabbed:
-			break
+
 		frame = imutils.resize(frame, width=500)
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		gray = cv2.GaussianBlur(gray, (21, 21), 0)
 		if avg is None:
+			print "Starting background model"
 			avg = gray.copy().astype("float")
+			rawCapture.truncate(0)
 			continue
 		cv2.accumulateWeighted(gray, avg, 0.5)
 		frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
 		thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
 		thresh = cv2.dilate(thresh, None, iterations=2)
 		(_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 		for c in cnts:
 			if cv2.contourArea(c) < conf["min_area"]:
 				continue
 			(x, y, w, h) = cv2.boundingRect(c)
 			cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
 			text = "Occupied"
-		cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-		cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-		cv2.imshow("Security Feed", frame)
-		cv2.imshow("Thresh", thresh)
-		cv2.imshow("Frame Delta", frameDelta)
-		key = cv2.waitKey(1) & 0xFF
-		if key == ord("q"):
-			break
+		ts = timestamp.strftime("%A %d %B %Y %H %M %S")
+		#cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+		if text == "Occupied":
+			if (timestamp - last_save).seconds >= conf["min_save_seconds"]:
+				motion_counter += 1
+				if motion_counter >= conf["min_motion_frames"]:
+					image_name = "{t_stamp}.jpg".format(t_stamp=ts)
+					cv2.imwrite('images/' + image_name, frame)
+					last_save = timestamp
+					motion_counter = 0
+					if (timestamp - last_MMS).seconds >= conf["min_upload_seconds"]:
+						a = multiprocessing.Process(target=send_mms, args=(image_name, conf))
+						a.start()
+						last_MMS = timestamp
+		else:
+			motion_counter = 0
+		if conf["show_video"]:
+			cv2.imshow("Security Feed", frame)
+			key = cv2.waitKey(1) & 0xFF
+			if key == ord("q"):
+				break
+		rawCapture.truncate(0)
 
 def send_mms(image_filename, conf):
 	now = dt.datetime.now()
@@ -66,8 +102,4 @@ if __name__=='__main__':
 	warnings.filterwarnings("ignore")
 	with open("conf.json", 'r') as f:
 		conf = json.load(f)
-	camera = cv2.VideoCapture(0)
-	time.sleep(0.25)
-	run_motion(camera, conf)
-	camera.release()
-	cv2.destroyAllWindows()
+	run_motion(conf)
